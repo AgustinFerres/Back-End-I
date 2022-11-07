7470ecf75870d665c0f61c2eacff6ffbe22385f1
AgustinFerres <agustinferres4403@gmail.com>
2022-11-07 17:46:41 -0300

    parcial fixed

--- clases/FERRES-AGUSTIN-PARCIAL/src/Cliente.java
+++ clases/FERRES-AGUSTIN-PARCIAL/src/Cliente.java
@@ -11,12 +11,22 @@ public class Cliente {
 
         BD.crearTablas();
         OdontologoService odontologoService = new OdontologoService();
+
         Odontologo odontologo1 = new Odontologo(1234,"Agustin","Ferres");
+        Odontologo odontologo2 = new Odontologo(1134,"Brahian","Tierno");
+        Odontologo odontologo3 = new Odontologo(2312,"Gonzalo","Alvarez");
+        Odontologo odontologo4 = new Odontologo(3212,"Rodrigo","Benitez");
+        Odontologo odontologo5 = new Odontologo(4212,"Javier","Paredes");
+
 
         odontologoService.guardar(odontologo1);
+        odontologoService.guardar(odontologo2);
+        odontologoService.guardar(odontologo3);
+        odontologoService.guardar(odontologo4);
+        odontologoService.guardar(odontologo5);
 
-        String resActual = odontologoService.buscar(1).toString();
-        String resEsperadal = "Agustin Ferres matricula: 1234";
+        Integer resActual = odontologoService.buscarTodos().size();
+        Integer resEsperadal = 5;
 
         Assertions.assertEquals(resEsperadal,resActual);
     }
