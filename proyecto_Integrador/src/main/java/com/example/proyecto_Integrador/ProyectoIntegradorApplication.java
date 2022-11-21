72194602b7f73ccc1cf08c860bcba63e602b3e04
AgustinFerres <agustinferres4403@gmail.com>
2022-11-21 08:13:37 -0300

    proyecto final fixing cors

--- proyecto_Integrador/src/main/java/com/example/proyecto_Integrador/ProyectoIntegradorApplication.java
+++ proyecto_Integrador/src/main/java/com/example/proyecto_Integrador/ProyectoIntegradorApplication.java
@@ -3,6 +3,10 @@ package com.example.proyecto_Integrador;
 import com.example.proyecto_Integrador.dao.BD;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class ProyectoIntegradorApplication {
@@ -11,5 +15,15 @@ public class ProyectoIntegradorApplication {
 		BD.crearTablas();
 		SpringApplication.run(ProyectoIntegradorApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/odontologos").allowedOrigins("http://127.0.0.1:5500");
+				registry.addMapping("/pacientes").allowedOrigins("http://127.0.0.1:5500");
+				registry.addMapping("/turnos").allowedOrigins("http://127.0.0.1:5500");
+			}
+		};
+	}
 }
